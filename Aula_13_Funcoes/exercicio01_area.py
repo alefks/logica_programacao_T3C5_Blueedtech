ef7bbe8253793383b551a873d7fe3be4bf9893e6
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2021-08-26 20:27:45 -0300

    Delete Aula_13_Funcoes directory

--- Aula_13_Funcoes/exercicio01_area.py
+++
@@ -1,9 +0,0 @@
-def area(largura, comprimento):
-    total = largura * comprimento
-
-    print(f"A área total é {total}")
-
-num1 = float(input("Digite a largura: "))
-num2 = float(input("Digite o comprimento: "))
-
-area(num1,num2)
