85da99bf34487b8548c4cdb4c4d0989e84534908
Alessandro Kobashigawa <ale_cheque_mate@hotmail.com>
2021-08-26 20:27:32 -0300

    Delete Aula_11 directory

--- Aula_11/exercicio_1_dicionarios.py
+++
@@ -1,9 +0,0 @@
-#Exercício 1 - Criar dicionário a partir de uma lista, e cada valor correspondente é N ao quadrado.
-
-numeros = [1, 4, 5, 6, 7, 9]
-dicnumeros1 = {}
-
-for i in numeros:
-    dicnumeros1[i] = i**2
-
-print(dicnumeros1)
--- Aula_11/exercicio_2_dicionarios.py
+++
@@ -1,8 +0,0 @@
-#Exercício 2 - Criar conjunto de 1 a 10, cada valor associado é o mesmo número ao quadrado
-
-dicnumeros2 = {}
-
-for x in range(1,11):
-    dicnumeros2 [x] = x**2
-
-print(dicnumeros2)
